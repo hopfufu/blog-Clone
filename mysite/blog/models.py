dd6de1754e0ce2726fd699d56c9facea58b6d5fa
hopfufu <91218375+hopfufu@users.noreply.github.com>
2022-07-21 07:54:53 +0530

    Update models.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,23 @@
+from pyexpat import model
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class post(models.Model):
+    author=models.ForeignKey('auth.User')
+    title=models.CharField(max_length=200)
+    text=models.TextField()
+    create_date=models.DateTimeField(default=timezone.now())
+    published_date=models.DateTimeField(blank=True,null=True)
+
+    def publish(self):
+        self.published_date=timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approve_comments=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(model.Model):
+    post =models.ForeignKey('blo')
